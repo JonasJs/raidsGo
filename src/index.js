b03423e8a96f48ec2917f863f7a15cb8cc240b63
Jonas Alves <joonasbalves@gmail.com>
2019-03-14 13:27:01 -0300

    Routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Style
+import './index.css';
+
+// Pages
+import App from './pages/App';
+import Login from './pages/Auth/Login';
+import Register from './pages/Auth/Register';
+
+
+const Root = () => (
+    <Router>
+        <Switch>
+            <Route exact path="/" component={App} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+        </Switch>        
+    </Router>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
