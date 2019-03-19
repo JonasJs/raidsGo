b5ff3ec14ae95a59e392b9426501975a68279385
Jonas Alves <joonasjss@gmail.com>
2019-03-19 01:46:20 -0300

    user in redux

--- src/index.js
+++ src/index.js
@@ -12,17 +12,28 @@ import App from './pages/App';
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
 
+// Redux
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux'; 
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+import { setUser } from './actions';
+
+const store = createStore( rootReducer, composeWithDevTools());
+
 
 class Root extends Component{
 
     componentDidMount = () => {
         firebase.auth().onAuthStateChanged(user => {
             if(user){
+                this.props.setUser(user);
                 this.props.history.push('/');
                 console.log(user)
             }
         });
     }
+
 
     render() {
         return (
@@ -36,12 +47,14 @@ class Root extends Component{
     }
 }
 
-const RootWithAuth = withRouter(Root);
+const RootWithAuth = withRouter(connect(null, { setUser})(Root));
 
 ReactDOM.render(
-    <Router>
-        <RootWithAuth />
-    </Router>,
+    <Provider store={store}>
+        <Router>
+            <RootWithAuth />
+        </Router>
+    </Provider>,
     document.getElementById('root')
 );
 registerServiceWorker();
